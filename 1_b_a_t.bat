24251636825a0aec61db87b3e6af7c51bfce7929
ttyson-adm <60528769+ttyson-adm@users.noreply.github.com>
2020-02-05 11:16:47 -0800

    Add files via upload

---
+++ 1_b_a_t.bat
@@ -0,0 +1,11 @@
+start /d "c:\program files\internet explorer" iexplore http://www-direct.crest-approved.org?bch=ping
+start /d "c:\program files\internet explorer" iexplore http://46.252.73.45?bch=ping
+
+rem start /d "c:\program files\internet explorer" iexplore https://www-direct.crest-approved.org?bch=ping
+rem start /d "c:\program files\internet explorer" iexplore https://46.252.73.45?bch=ping
+
+start /d "c:\program files\internet explorer" iexplore http://www-direct.crest-approved.org:8080?bch=ping
+start /d "c:\program files\internet explorer" iexplore http://46.252.73.45:8080?bch=ping
+
+start /d "c:\program files\internet explorer" iexplore http://www-direct.crest-approved.org:53?bch=ping
+start /d "c:\program files\internet explorer" iexplore http://46.252.73.45:53?bch=ping
